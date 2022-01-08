0c3b18c96fe1f87928d8986a67300b12c800cba4
sun <ablocker@163.com>
2022-01-08 11:25:04 +0800

    'bug'

--- gen-key.sh
+++ gen-key.sh
@@ -1,2 +1,2 @@
 openssl genrsa -out equipment-back/keys/rsa_private_key.pem 2048
-openssl rsa -in equipment-back/keys/rsa_private_key.pem -pubout -out keys/rsa_public_key.pem
+openssl rsa -in equipment-back/keys/rsa_private_key.pem -pubout -out equipment-back/keys/rsa_public_key.pem

07b00039c98bef0bc9eb6dd93dedc9a1e805df26
sun <ablocker@163.com>
2022-01-08 13:53:49 +0800

    '启用gzip'

--- equipment-front/vue.config.js
+++ equipment-front/vue.config.js
@@ -1,3 +1,10 @@
+const webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin')
+const path = require('path')
+const zlib = require('zlib')
+
+// const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
     publicPath: './',
     filenameHashing: true,
@@ -21,5 +28,35 @@ module.exports = {
                 changeOrigin: true
             }
         }
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+                '@i': path.resolve(__dirname, './src/assets'),
+            }
+        },
+        plugins: [
+            new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+            new CompressionPlugin({
+                filename: "[path][base].gz",
+                algorithm: "gzip",
+                test: /\.js$|\.css$|\.html$/,
+                threshold: 10240,
+                minRatio: 0.8
+            }),
+            new CompressionPlugin({
+                filename: "[path][base].br",
+                algorithm: "brotliCompress",
+                test: /\.(js|css|html|svg)$/,
+                compressionOptions: {
+                    params: {
+                        [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+                    },
+                },
+                threshold: 10240,
+                minRatio: 0.8
+            })
+        ]
     }
 }

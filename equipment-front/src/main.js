e669a2cc843b88b4a9037e6b7263a3dc23425b68
sun <ablocker@163.com>
2022-01-08 11:50:55 +0800

    按需引入

--- equipment-front/src/main.js
+++ equipment-front/src/main.js
@@ -2,26 +2,38 @@ import Vue from 'vue'
 import { BootstrapVue, CardPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import Antd from 'ant-design-vue'
+import { Button, message, Form, Menu, Layout, Card, Col, Row, Upload, Input, Icon  } from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 import ECharts from 'vue-echarts'
 import { use } from 'echarts/core'
 
 import { CanvasRenderer } from 'echarts/renderers'
-import { BarChart, PieChart, LineChart } from 'echarts/charts'
+import { LineChart } from 'echarts/charts'
 import { GridComponent, TooltipComponent, LegendComponent } from 'echarts/components'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.config.productionTip = false
+
 Vue.use(BootstrapVue)
 Vue.use(CardPlugin)
-Vue.use(Antd)
+
+Vue.use(Button)
+Vue.prototype.$message = message
+Vue.use(Form)
+Vue.use(Upload)
+Vue.use(Input)
+Vue.use(Icon)
+Vue.use(Menu)
+Vue.use(Layout)
+Vue.use(Card)
+Vue.use(Col)
+Vue.use(Row)
+
 use([
   CanvasRenderer,
-  BarChart,
-  PieChart,
   LineChart,
   GridComponent,
   TooltipComponent,
@@ -29,7 +41,6 @@ use([
 ])
 Vue.component('v-chart', ECharts)
 
-Vue.config.productionTip = false
 
 new Vue({
   router,

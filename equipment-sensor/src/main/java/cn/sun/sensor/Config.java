5bb9b45ab36b13bce98bff224d5d43b61fc43601
sun <ablocker@163.com>
2022-04-15 18:12:44 +0800

    docker-kafka

--- equipment-sensor/src/main/java/cn/sun/sensor/Config.java
+++ equipment-sensor/src/main/java/cn/sun/sensor/Config.java
@@ -4,7 +4,7 @@ public class Config
 {
     public static final int port = 8083;
     public static final String paramPath = "param.dat";
-    public static final String kafkaServer = "www.ablocker.top:9092";
+    public static final String kafkaServer = "equipment-kafka:9092";
     public static final String kafkaTopic = "sensor";
     public static final String KafkaKey = "sensor";
     public static final String kafkaClientId = "equipment-sensor";
